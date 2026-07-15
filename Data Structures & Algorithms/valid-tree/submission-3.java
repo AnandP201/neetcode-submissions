2c00a2e9ec7542129cf4eea91b964b6a4252275b
paicodes12 <81501797+AnandP201@users.noreply.github.com>
2026-07-15 20:43:57 +0530

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.java
@@ -0,0 +1,56 @@
+class DSU {
+    int parent[], rank[];
+
+    public DSU(int V) {
+        this.parent = new int[V];
+        this.rank = new int[V];
+        for (int i = 0; i < V; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = find(parent[i]);
+    }
+
+    public boolean union(int u, int v) {
+        int pu = find(u), pv = find(v);
+
+        if (pu == pv)
+            return false;
+
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else if (rank[pv] < rank[pu]) {
+            parent[pv] = pu;
+        } else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // Edges form a tree , if they all connect to form exact 1 component and without any cycle
+
+        int connected = n;
+
+        DSU dsu = new DSU(n);
+
+        for (int edge[] : edges) {
+            int u = edge[0], v = edge[1];
+            if (dsu.union(u, v)) {
+                connected--;
+            } else {
+                return false;
+            }
+        }
+
+        return connected == 1;
+    }
+}
